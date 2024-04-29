c2f8619b26d112ec228fa835f638be9baca5fb22
kchiwakaya <kchiwakaya@gmail.com>
2024-04-29 11:26:05 +0200

    hie

--- limsregister/models.py
+++ limsregister/models.py
@@ -1,6 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Province(models.Model):
+    Province_code = models.CharField(max_length =50,primary_key=True,default="Hello")
+    Province_Name = models.CharField(max_length=50)
+    Province_Number = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.Province_Name
+class District(models.Model):
+    District_code = models.CharField(max_length=50,primary_key=True)
+    District_Name = models.CharField(max_length=50)
+    District_Number = models.CharField(max_length=50)    
+
+    def __str__(self):
+        return self.District_Name
+class Old_District(models.Model):
+    Old_Number = models.CharField(max_length=50)  
+
 class FarmRegister(models.Model):
     farm_id = models.CharField(max_length=11,primary_key=True)
     farm_name = models.CharField(max_length=500)
@@ -15,7 +32,7 @@ class FarmRegister(models.Model):
     diagram_no = models.CharField(max_length=50)
     title_type = models.CharField( max_length=50)
     deed_no = models.CharField(max_length=50)
-    province = models.CharField(max_length=50)
+    province_code = models.ForeignKey(Province,on_delete=models.CASCADE)
     previous_district = models.CharField(max_length=70)
     current_district = models.CharField(max_length=50)
     farm_activity = models.CharField(max_length=50)
